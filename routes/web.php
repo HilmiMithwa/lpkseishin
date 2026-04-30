14f9bb2aebf9da1128c5ab91468ad6224cc9b702
HilmiMithwa <hilmimithwa@gmail.com>
2026-04-30 17:46:41 +0700

    authenticated 3 role users [admin, siswa, guru]

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,28 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SesiController;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
+Route::get('/', [SesiController::class, 'index']);
+
+Route::get('/dashboard', function() {
+
+    $role = Auth::user()->role;
+
+    if ($role === 'admin') {
+        return redirect()->route('admin.dashboard');
+    }
+
+    if ($role === 'siswa') {
+        return redirect()->route('students.dashboard');
+    }
+
+    if ($role === 'guru') {
+        return redirect()->route('teacher.dashboard');
+    }
+    
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -16,5 +32,27 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//Dashboard Admin
+Route::middleware(['auth', 'checkRole:admin'])->group(function () {
+    Route::get('/admin/dashboard', function() {
+        return view ('admin.dashboard');
+    })->name('admin.dashboard');
+});
+
+//Dashboard Siswa
+Route::middleware(['auth', 'checkRole:siswa'])->group(function () {
+    Route::get('/students/dashboard', function() {
+        return view ('students.dashboard');
+    })->name('students.dashboard');
+});
+
+//Dashboard Guru
+Route::middleware(['auth', 'checkRole:guru'])->group(function () {
+    Route::get('/teacher/dashboard', function() {
+        return view ('teacher.dashboard');
+    })->name('teacher.dashboard');
+});
+
 
 require __DIR__.'/auth.php';
