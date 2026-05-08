59e6ede4451d2cbc9e48978518b1420d9bd960df
HilmiMithwa <hilmimithwa@gmail.com>
2026-05-08 09:08:32 +0700

    setting up StudentController

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Mapel;
 
-class MapelController extends Controller
+class StudentController extends Controller
 {
     public function index()
     {
