e30139900cf3c58902af75e1a72b6003e27a1c15
HilmiMithwa <hilmimithwa@gmail.com>
2026-05-21 06:13:34 +0700

    creating pengiriman_tugas model

--- app/Models/Pengiriman_Tugas.php
+++ app/Models/Pengiriman_Tugas.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pengiriman_Tugas extends Model
 {
-    //
+    protected $table = 'pengiriman_tugas';
+    protected $primaryKey = 'id_pengiriman_tugas';
+
+    protected $fillable = [
+        'text_content',
+        'file_path',
+        'submitted_at',
+        'status',
+        'nilai',
+        'id_tugas',
+        'id_user'
+    ];
+
+    public function tugas()
+    {
+        return $this->belongsTo(Tugas::class, 'id_tugas', 'id_tugas');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id_user');
+    }
+    
 }
