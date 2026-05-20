71b55e8475644fa4aee0e33d14e1389f4e8612d4
HilmiMithwa <hilmimithwa@gmail.com>
2026-05-21 05:55:46 +0700

    creating storemodulerequest

--- app/Http/Controllers/Student/ModulController.php
+++ app/Http/Controllers/Student/ModulController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Mapel;
 use App\Models\Modul;
+use App\Http\Requests\Student\StoreModulRequest;
 
 //Nunggu dashboard guru keluar
 class ModulController extends Controller
@@ -21,21 +22,9 @@ class ModulController extends Controller
 
     //beberapa validasi untuk dashbaord guru
 
-    public function store(Request $request)
+    public function store(StoreModulRequest $request)
     {
-        $request->validate([
-            'nama_modul' => 'required|string|max:50',
-            'kode_modul' => 'required|string|unique:modul,kode_modul',
-            'teori' => 'required|integer',
-            'praktik' => 'required|integer',
-            'module_description' => 'nullable|max:100',
-            'id_mapel' => 'required|exists:mapel, id_mapel',
-            'id_rps' => 'required|exists:rps, id_rps'
-        ]);
-
-        Modul::create($request->all());
+        Modul::create($request->validated());
         return redirect()->route('teacher.dashboardmodul')->with('success', 'Modul Berhasil ditambahkan!'); //buat routingan dashboard guru ini aku serahin lagi ke kamu zan. Ini sebagai contoh doang
-        
-    
     }
 }
