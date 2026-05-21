58eda04dec37dd0611050db2f52505644fdd35cb
HilmiMithwa <hilmimithwa@gmail.com>
2026-05-22 00:02:35 +0700

    revising modulController and Request ModulStore for teacher

--- app/Http/Controllers/Student/ModulController.php
+++ app/Http/Controllers/Student/ModulController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Mapel;
 use App\Models\Modul;
-use App\Http\Requests\Student\StoreModulRequest;
+
 
 //Nunggu dashboard guru keluar
 class ModulController extends Controller
@@ -20,13 +20,7 @@ class ModulController extends Controller
         return view('students.module-detail', compact('modul'));
     }
 
-    //beberapa validasi untuk dashbaord guru
-
-    public function store(StoreModulRequest $request)
-    {
-        Modul::create($request->validated());
-        return redirect()->route('teacher.dashboardmodul')->with('success', 'Modul Berhasil ditambahkan!'); //buat routingan dashboard guru ini aku serahin lagi ke kamu zan. Ini sebagai contoh doang
-    }
+    
 
     
 }
