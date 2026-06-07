1c44bd7ee30335c0df35fb38baa424381de27a74
HilmiMithwa <hilmimithwa@gmail.com>
2026-06-07 22:01:01 +0700

    trusting the proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'checkRole' => \App\Http\Middleware\RoleManager::class,
         ]);
+
+        // Tambahkan baris ini untuk mengatasi Mixed Content di Railway
+        $middleware->trustProxies(at: '*'); 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
