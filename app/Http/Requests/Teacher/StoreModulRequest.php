d6eee56b4b1a200c7c10187a3b57dfc7673099c7
HilmiMithwa <hilmimithwa@gmail.com>
2026-06-07 17:25:29 +0700

    fixing try catch on adding module

--- app/Http/Requests/Teacher/StoreModulRequest.php
+++ app/Http/Requests/Teacher/StoreModulRequest.php
@@ -25,10 +25,10 @@ class StoreModulRequest extends FormRequest
         return [
             'nama_modul' => 'required|string|max:255',
             'kode_modul' => 'nullable|string|unique:modul,kode_modul',
-            'teori' => 'nullable|integer|min:0',
-            'praktik' => 'nullable|integer|min:0',
+            'teori' => 'required|integer|min:0',
+            'praktik' => 'required|integer|min:0',
             'jp' => 'nullable|integer|min:0', // Alokasi Durasi (JP)
-            'module_description' => 'nullable|string',
+            'module_description' => 'required|string',
             'id_mapel' => 'required|exists:mapel,id_mapel',
             'id_rps' => 'nullable|exists:rps,id_rps',
             'icon_type' => 'nullable|integer|in:1,2,3,4',
@@ -41,8 +41,9 @@ class StoreModulRequest extends FormRequest
         return [
             'nama_modul.required' => 'Nama Modul harus diisi',
             'kode_modul.unique' => 'Kode modul harus unik!',
-            'teori.required' => 'Jumlah teori harus diisi!',
-            'praktik.required' => 'Jumlah praktik harus diisi!'
+            'teori.required' => 'Jumlah JP Teori harus diisi!',
+            'praktik.required' => 'Jumlah JP Praktik harus diisi!',
+            'module_description.required' => 'Deskripsi Modul tidak boleh kosong!'
         ];
     }
 }
